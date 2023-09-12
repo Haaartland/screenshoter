8ece7b59556eee7a1b3273f5302abafac3f851f3
Patrik Engborg <patrik@engborg.nu>
2023-09-12 09:12:32 +0200

    Add headless: 'new' as arg to puppeteer.launch

--- screenShoter.js
+++ screenShoter.js
@@ -1,47 +1,48 @@
-const puppeteer = require('puppeteer');
-require('dotenv').config();
+const puppeteer = require('puppeteer')
+require('dotenv').config()
 const screenShoter = async (req, res) => {
+  const { adId, type, baseUrl } = req.body
+  let browser = null
 
-  const { adId, type, baseUrl } = req.body;
-  let browser = null;
-
-  let base64 = '';
+  let base64 = ''
 
   try {
     browser = await puppeteer.launch({
+      headless: 'new',
       args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote"
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        '--single-process',
+        '--no-zygote',
       ],
-      executablePath: process.env.NODE_ENV === 'production'
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath()
-    });
+      executablePath:
+        process.env.NODE_ENV === 'production'
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    })
 
-    const page = await browser.newPage();
+    const page = await browser.newPage()
 
-    await page.setViewport({ width: 1440, height: 1080, deviceScaleFactor: 2 });
+    await page.setViewport({ width: 1440, height: 1080, deviceScaleFactor: 2 })
 
-    await page.goto(`${baseUrl}/deals/banners/${adId}?type=${type}`);
+    await page.goto(`${baseUrl}/deals/banners/${adId}?type=${type}`)
 
     // const readMoreBtn = await page.waitForSelector('.btn-read-more')
     // await readMoreBtn.evaluate((el) => el.remove())
 
-    const element = await page.$(`.ad--${type}`);
+    const element = await page.$(`.ad--${type}`)
 
-    base64 = await element.screenshot({ encoding: 'base64' });
+    base64 = await element.screenshot({ encoding: 'base64' })
   } catch (err) {
-    console.error(`❌ Error: ${err.message}`);
-    res.send(`Something went wrong: ${err}`);
+    console.error(`❌ Error: ${err.message}`)
+    res.send(`Something went wrong: ${err}`)
   } finally {
-    await browser.close();
-    console.log(`\n🎉 Screenshot captured.`);
+    await browser.close()
+    console.log(`\n🎉 Screenshot captured.`)
   }
 
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ base64 }));
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ base64 }))
 }
 
-module.exports = screenShoter;
+module.exports = screenShoter
